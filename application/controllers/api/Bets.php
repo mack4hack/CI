4289d15bd3616f144bfede4fc8644a11b6635758
Makarand Uparwat <makarand.uparwat@iniksha.com>
2015-11-19 16:59:59 +0530

    seperate function added to place Bet

--- application/controllers/api/Bets.php
+++ application/controllers/api/Bets.php
@@ -21,14 +21,12 @@
 			echo "you are in Bets controller";	
 		}
 
-		public function PlaceBet_post()
+		public function PlaceBetFirst_post()
 		{
 			$payout = ($this->post('bet_amount')*8.5);
-			if($this->post('digit') > 9)
-				$payout = ($this->post('bet_amount')*85);
 
 			$data = array(
-				'game_type'=>$this->post('game_type'),
+				'game_type'=>1,
 				'player_id'=>$this->post('player_id'),
 				'digit'=>$this->post('digit'),
 				'bet'=>1,
@@ -37,8 +35,99 @@
 				'timeslot' => date('Y-m-d H:i:s')
 				); 
 
+			$history = array(
+				'game_type'=>1,
+				'player_id'=>$this->post('player_id'),
+				'bet_amount'=>$this->post('bet_amount'),
+				'first_digit'=>$this->post('digit'),
+				'payout'=>$payout,
+				'timeslot' => date('Y-m-d H:i:s')
+				);
+
 			if($this->Bets_model->placebet($data))
 			{	                
+				$this->Bets_model->addplayerhistory($history);
+
+				$this->response([
+					'status' => TRUE,
+					'message' => 'Bets Placed Successfully'
+				], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+				            
+			}else{   
+				$this->response([
+					'status' => FALSE,
+					'message' => 'Bets Cannot Be Placed!'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+
+		}
+		public function PlaceBetSecond_post()
+		{
+			$payout = ($this->post('bet_amount')*8.5);
+			
+			$data = array(
+				'game_type'=>2,
+				'player_id'=>$this->post('player_id'),
+				'digit'=>$this->post('digit'),
+				'bet'=>1,
+				'bet_amount'=>$this->post('bet_amount'),
+				'payout'=>$payout,
+				'timeslot' => date('Y-m-d H:i:s')
+				); 
+
+			$history = array(
+				'game_type'=>2,
+				'player_id'=>$this->post('player_id'),
+				'bet_amount'=>$this->post('bet_amount'),
+				'second_digit'=>$this->post('digit'),
+				'payout'=>$payout,
+				'timeslot' => date('Y-m-d H:i:s')
+				);
+
+			if($this->Bets_model->placebet($data))
+			{	                
+				$this->Bets_model->addplayerhistory($history);
+
+				$this->response([
+					'status' => TRUE,
+					'message' => 'Bets Placed Successfully'
+				], REST_Controller::HTTP_OK); // NOT_FOUND (404) being the HTTP response code
+				            
+			}else{   
+				$this->response([
+					'status' => FALSE,
+					'message' => 'Bets Cannot Be Placed!'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+
+		}
+		public function PlaceBetJodi_post()
+		{
+			$payout = ($this->post('bet_amount')*85);
+
+			$data = array(
+				'game_type'=>3,
+				'player_id'=>$this->post('player_id'),
+				'digit'=>$this->post('digit'),
+				'bet'=>1,
+				'bet_amount'=>$this->post('bet_amount'),
+				'payout'=>$payout,
+				'timeslot' => date('Y-m-d H:i:s')
+				); 
+
+			$history = array(
+				'game_type'=>3,
+				'player_id'=>$this->post('player_id'),
+				'bet_amount'=>$this->post('bet_amount'),
+				'jodi_digit'=>$this->post('digit'),
+				'payout'=>$payout,
+				'timeslot' => date('Y-m-d H:i:s')
+				);
+
+			if($this->Bets_model->placebet($data))
+			{	                
+				$this->Bets_model->addplayerhistory($history);
+
 				$this->response([
 					'status' => TRUE,
 					'message' => 'Bets Placed Successfully'
