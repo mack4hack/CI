a240b62da7d3263f18febafc2d3d9c90ed01fb59
Ashish <ashish.bubne@iniksha.com>
2015-11-29 10:41:45 +0530

    auth login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,7 +18,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- BEGIN HEAD -->
 <head>
 <meta charset="utf-8"/>
-<title>GamePlay | Login Options </title>
+<title>MoneyPlay | Login Options </title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
@@ -50,7 +50,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- BEGIN LOGO -->
 <div class="logo">
   <a href="index.html">
-  <img src="../assets/admin/layout/img/logo-big.png" alt=""/>
+  <img src="../assets/admin/layout/img/logo1.png" alt=""/>
   </a>
 </div>
 <!-- END LOGO -->
@@ -100,7 +100,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- END LOGIN -->
 <!-- BEGIN COPYRIGHT -->
 <div class="copyright">
-   2014 &copy; Metronic. Admin Dashboard Template.
+   2015 &copy; MoneyPlay. Admin Dashboard Template.
 </div>
 <!-- END COPYRIGHT -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
